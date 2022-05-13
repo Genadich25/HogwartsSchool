8cb23d4ec4f8908500c4132650c49210927281a7
pin20 <pin200048@gmail.com>
2022-05-13 22:32:45 +0300

    creating avatar support

--- src/main/java/ru/hogwarts/school/controller/StudentController.java
+++ src/main/java/ru/hogwarts/school/controller/StudentController.java
@@ -1,11 +1,23 @@
 package ru.hogwarts.school.controller;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import ru.hogwarts.school.model.Avatar;
 import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.service.AvatarService;
 import ru.hogwarts.school.service.StudentService;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URI;
+import java.nio.file.*;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,9 +26,11 @@ import java.util.Optional;
 public class StudentController {
 
     private final StudentService service;
+    private final AvatarService avatarService;
 
-    public StudentController(StudentService service) {
+    public StudentController(StudentService service, AvatarService avatarService) {
         this.service = service;
+        this.avatarService = avatarService;
     }
 
     @PostMapping
@@ -24,7 +38,7 @@ public class StudentController {
         return service.createStudent(student);
     }
 
-    @GetMapping("{id}")
+    @GetMapping(value = "{id}")
     public Optional<Student> findStudent(@PathVariable Long id){
         return service.findStudent(id);
     }
@@ -34,7 +48,7 @@ public class StudentController {
         return service.editStudent(student);
     }
 
-    @DeleteMapping("{id}")
+    @DeleteMapping(value = "{id}")
     public ResponseEntity removeStudent(@PathVariable Long id){
         service.deleteStudent(id);
         return ResponseEntity.ok().build();
@@ -45,14 +59,49 @@ public class StudentController {
         return service.filterStudentByAge(age);
     }
 
-    @GetMapping("/find")
+    @GetMapping(value = "/find")
     public List<Student> findByAgeBetween(@RequestParam int min,@RequestParam int max){
         return service.findByAgeBetween(min, max);
     }
 
-    @GetMapping("{id}/faculty")
+    @GetMapping(value = "{id}/faculty")
     public Faculty getFaculty(@PathVariable Long id){
         return service.getFaculty(id);
     }
 
+    @PostMapping(value = "{id}/avatar", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> uploadAvatar(@PathVariable Long id, @RequestParam MultipartFile avatar) throws IOException {
+        if(avatar.getSize() >= 1024 * 300) { // 300 Kb
+            return ResponseEntity.badRequest().body("File is too big!");
+        }
+        avatarService.uploadAvatar(id, avatar);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping(value = "{id}/avatar/preview")
+    public ResponseEntity<byte[]> downloadAvatar(@PathVariable Long id){
+        Avatar avatar = avatarService.findAvatar(id);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.parseMediaType(avatar.getMediaType()));
+        headers.setContentLength(avatar.getData().length);
+
+        return ResponseEntity.status(HttpStatus.OK).headers(headers).body(avatar.getData());
+    }
+
+    @GetMapping(value = "{id}/avatar")
+    public void downloadAvatar(@PathVariable Long id, HttpServletResponse response) throws IOException {
+        Avatar avatar = avatarService.findAvatar(id);
+
+        Path path = Path.of(avatar.getFilePath());
+
+        try(InputStream is = Files.newInputStream(path);
+        OutputStream os = response.getOutputStream();){
+            response.setStatus(200);
+            response.setContentType(avatar.getMediaType());
+            response.setContentLength((int) avatar.getFileSize());
+            is.transferTo(os);
+        }
+
+    }
 }
